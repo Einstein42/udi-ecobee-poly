ce3e7331aab555b52fa2f0c4092ade5b3305fd4c
JimBo <jimboca3@gmail.com>
2018-12-28 13:59:17 -0800

    Update ecobee-poly.py

--- ecobee-poly.py
+++ ecobee-poly.py
@@ -259,7 +259,7 @@ class Controller(polyinterface.Controller):
                     self.addNode(Thermostat(self, address, address, thermostatId,
                                             'Ecobee - {}'.format(get_valid_node_name(thermostat['name'])),
                                             thermostat, fullData, useCelsius))
-            LOGGER.debug("discover: {}".format(son.dumps(self.getThermostatFull(thermostatId), sort_keys=True, indent=2)))
+            LOGGER.debug("discover: {}".format(json.dumps(self.getThermostatFull(thermostatId), sort_keys=True, indent=2)))
         self.discover_st = True
         self.in_discover = False
         return True
