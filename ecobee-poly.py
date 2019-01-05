e55f03b14d55eb8753b4954e27b2792df7a42122
JimBo <jimboca3@gmail.com>
2019-01-04 16:57:17 -0800

    Update ecobee-poly.py

--- ecobee-poly.py
+++ ecobee-poly.py
@@ -310,8 +310,7 @@ class Controller(polyinterface.Controller):
             self.update_profile = False
         else:
             self.update_profile = True
-            self.poly.installprofile()
-        self.l_info('check_profile','update_profile={}'.format(self.update_profile))
+        LOGGER.info('check_profile','update_profile={}'.format(self.update_profile))
         cdata['profile_info'] = self.profile_info
         self.saveCustomData(cdata)
         return self.update_profile
